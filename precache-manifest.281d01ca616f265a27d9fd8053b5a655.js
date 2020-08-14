2791ee14ae06a82808bfc997ad2a276fcfd4fd95
Mihir06121 <mp06121@gmail.com>
2020-08-14 09:21:24 +0530

    Updates

---
+++ precache-manifest.281d01ca616f265a27d9fd8053b5a655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43c1c6b35875f31a3ee909d972b4d0b",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "5d9c4f2446d784c45972",
+    "url": "/youtube-clone/static/css/main.e746e14d.chunk.css"
+  },
+  {
+    "revision": "042984e06cb22b533a22",
+    "url": "/youtube-clone/static/js/2.67b13fc8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/youtube-clone/static/js/2.67b13fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9c4f2446d784c45972",
+    "url": "/youtube-clone/static/js/main.0c7d0881.chunk.js"
+  },
+  {
+    "revision": "499551069bc6d648d2df",
+    "url": "/youtube-clone/static/js/runtime-main.6ca2da9d.js"
+  }
+]);
